e789b22691c7b5b9b139781660d2afa9666ca891
Maxim <you@example.com>
2020-11-19 19:02:40 +0300

    made some correction

--- src/com/Main.java
+++ src/com/Main.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         StartProcess start = new StartProcess();
 
